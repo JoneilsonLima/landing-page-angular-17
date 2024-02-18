0e6e5a8964892a4c225d41354df1f4c968254dcf
joneilson pereira <joneilson84@gmail.com>
2024-02-18 14:41:47 -0300

    feat: criando componente de botão

---
+++ src/app/components/btn-primary/btn-primary.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+type BtnVariants = 'primary' | 'secondary';
+
+@Component({
+  selector: 'app-btn-primary',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './btn-primary.component.html',
+  styleUrl: './btn-primary.component.scss'
+})
+export class BtnPrimaryComponent {
+  @Input() btnText: string = '';
+  @Input() disabled: boolean = false;
+  @Input() loading: boolean = false;
+  @Input() variant: BtnVariants = 'primary';
+
+  @Output() onSubmit = new EventEmitter();
+
+  submit(): void {
+    this.onSubmit.emit();
+  }
+}
