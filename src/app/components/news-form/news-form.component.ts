3ba772152a726c50915a4626bf917ef0c95fcea1
joneilson pereira <joneilson84@gmail.com>
2024-02-24 11:55:31 -0300

    feat: configurando formulário reativo

--- src/app/components/news-form/news-form.component.ts
+++ src/app/components/news-form/news-form.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-news-form',
   standalone: true,
-  imports: [BtnPrimaryComponent],
+  imports: [
+    BtnPrimaryComponent,
+    ReactiveFormsModule
+  ],
   templateUrl: './news-form.component.html',
   styleUrl: './news-form.component.scss'
 })
 export class NewsFormComponent {
+  public formGroup!: FormGroup;
 
+  constructor() {
+    this.formGroup = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email])
+    })
+  }
+
+  onSubimit(): void {
+    console.log(this.formGroup.value);
+  }
 }
