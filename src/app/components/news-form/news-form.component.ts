ca57c18e64f2bd37c08d707e537dc7d6e5a690be
joneilson pereira <joneilson84@gmail.com>
2024-02-24 13:22:32 -0300

    service: serviço para cadastrar dados do formulário

--- src/app/components/news-form/news-form.component.ts
+++ src/app/components/news-form/news-form.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NewsletterService } from '../../services/newsletter.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-news-form',
@@ -9,13 +11,17 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
     BtnPrimaryComponent,
     ReactiveFormsModule
   ],
+  providers: [
+    NewsletterService
+  ],
   templateUrl: './news-form.component.html',
   styleUrl: './news-form.component.scss'
 })
 export class NewsFormComponent {
   public formGroup!: FormGroup;
+  public loading = signal(false);
 
-  constructor() {
+  constructor(private service: NewsletterService) {
     this.formGroup = new FormGroup({
       name: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email])
@@ -23,6 +29,18 @@ export class NewsFormComponent {
   }
 
   onSubimit(): void {
-    console.log(this.formGroup.value);
+    if (this.formGroup.valid) {
+      this.loading.set(true);
+      const {name, email} = this.formGroup.getRawValue()
+      this.service.sendData(name, email).subscribe({
+        next: (data) => {
+          this.formGroup.reset()
+          this.loading.set(false);
+        },
+        error: (error) => {
+          console.log(error)
+        }
+      })
+    }
   }
 }
--- src/app/services/newsletter.service.ts
+++ src/app/services/newsletter.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { NewsletterResponse } from '../components/interfaces/newsletter-response.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsletterService {
+  private readonly endpointUrl = 'https://faed47pcwb7biktidlecuafuty0aegep.lambda-url.us-east-1.on.aws/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  sendData(name: string, email: string): Observable<NewsletterResponse> {
+    const data = {name, email};
+
+    return this.http.post<NewsletterResponse>(this.endpointUrl, data);
+  }
 }
